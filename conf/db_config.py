0463af1f110fa4e64278c75004ba4facbbcfa943
panda <tharaniesh46@gamil.com.com>
2025-09-19 17:07:00 +0530

    Fix: keep only root requirements.txt and added gunicorn

--- conf/db_config.py
+++ conf/db_config.py
@@ -1,9 +1,9 @@
-import mysql.connector
+# import mysql.connector
 
-def get_db_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",        # change to your MySQL username
-        password="1234", # change to your MySQL password
-        database="test_db"   # change to your DB name
-    )
+# def get_db_connection():
+#     return mysql.connector.connect(
+#         host="localhost",
+#         user="root",        # change to your MySQL username
+#         password="1234", # change to your MySQL password
+#         database="test_db"   # change to your DB name
+#     )
